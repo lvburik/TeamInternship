4bf6f002aeee1725dc229a084b4092d94749e364
Luc van Burik <l.j.v.burik@student.tue.nl>
2025-03-13 15:09:26 +0100

    Update material parameters and add Emissivity

--- FEM.m
+++ FEM.m
@@ -1,5 +1,16 @@
 clear
 
+%environment properties
+Ambient_T = 22; %[°C]
+
+%material properties
+conductivity = 0.38;% [W/m*°K]
+density = 980; %[kg/m^3]
+specific_heat = 1200; %[J/°K]
+
+convex_coeff = 40; %[W/m*°K]
+emis_coeff = 0.9;
+
 %%define the model
 ThermalModel = createpde('thermal', 'transient');
 
@@ -10,19 +21,20 @@ Circular_damage
 pdegplot(ThermalModel,'FaceLabels','on','FaceAlpha',0.5)
 
 %material properties
-thermalProperties(ThermalModel,'ThermalConductivity',1,...
-                               'MassDensity',1,...
-                               'SpecificHeat',1);
+thermalProperties(ThermalModel,'ThermalConductivity',conductivity,...
+                               'MassDensity',density,...
+                               'SpecificHeat',specific_heat);
 
 %boundary conditions:
-thermalBC(ThermalModel,'Face',1:ThermalModel.Geometry.NumFaces,'ConvectionCoefficient',5, 'AmbientTemperature',22);
+thermalBC(ThermalModel,'Face',1:ThermalModel.Geometry.NumFaces,'ConvectionCoefficient',convex_coeff, 'AmbientTemperature',Ambient_T);
+ThermalModel.StefanBoltzmannConstant = 5.670373E-8;
+thermalBC(ThermalModel, 'Face', 1:ThermalModel.Geometry.NumFaces, 'Emissivity',emis_coeff, 'AmbientTemperature', Ambient_T);
 
 
-heatFlux = @(x, y) x + y;
-thermalBC(ThermalModel,'Face',[1, 2],'HeatFlux',@(location, state) heatFlux(location.x, location.y));
+thermalBC(ThermalModel,'Face',[1, 2],'HeatFlux',@heatFluxFunction);
 
 %initial conditions
-thermalIC(ThermalModel,0);
+thermalIC(ThermalModel,Ambient_T);
 
 %generate and display mesh
 generateMesh(ThermalModel, Hmax = 0.01);
@@ -30,10 +42,21 @@ figure
 pdemesh(ThermalModel)
 
 %%solve
-tlist = [0 0.1];
+tlist = 0:.5:600;
 
 thermalresults = solve(ThermalModel,tlist);
 
+function q = heatFluxFunction(region, state)
+    x = region.x; % X-coordinates of the surface
+    y = region.y; % Y-coordinates of the surface
+    t = state.time; % Current simulation time
 
+    
+    q = 1100;
+
+    if t > 300
+        q = 0;
+    end
+end
 
 
