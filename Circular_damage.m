017a5307824b415dcd50950e5a58178e033e425a
kpenners <k.penners@student.tue.nl>
2025-03-15 20:47:22 +0100

    add square defect functionality

--- Circular_damage.m
+++ Circular_damage.m
@@ -1,13 +1,16 @@
-%damage dimension
-radius = 0.02;
-d = 0.02;
-offset_x = .0;
-offset_y = .0;
-
-% load damage polyshape
-theta = linspace(0, 2*pi, 100);  % Angle for circle
-
-damage_x = offset_x + radius * cos(theta);  % X coordinates of the circle
-damage_y = offset_y + radius * sin(theta);  % Y coordinates of the circle
-
-damage = polyshape(damage_x, damage_y);
+function [damage, inner_edges] = Circular_damage
+    %damage dimension
+    radius = 0.02;
+    d = 0.02;
+    offset_x = .0;
+    offset_y = .0;
+    inner_edges = 5; % inner edges to add face
+    
+    % load damage polyshape
+    theta = linspace(0, 2*pi, 100);  % Angle for circle
+    
+    damage_x = offset_x + radius * cos(theta);  % X coordinates of the circle
+    damage_y = offset_y + radius * sin(theta);  % Y coordinates of the circle
+    
+    damage = polyshape(damage_x, damage_y);
+end
--- Square_damage.m
+++ Square_damage.m
@@ -1,11 +1,14 @@
-%damage dimension
-
-%%TODO (define other parameters)
-x_offset = 0;
-y_offset = 0;
-
-% load damage polyshape
-
-%%TODO (define corners of polyshape)
-
-damage = polyshape();%polyshape defining the damage geometry
+function [damage, inner_edges] = Square_damage
+    %damage dimension
+    length = 0.03;
+    offset_x = -0.0;
+    offset_y = -0.0;
+    inner_edges = [2,3,7,8]; % inner edges to add face
+    
+    %define corners of polyshape
+    damage_x = [-1 1 1 -1]*length/2 + offset_x;
+    damage_y = [-1 -1 1 1]*length/2 + offset_y;
+    
+    %load damage polyshape
+    damage = polyshape(damage_x, damage_y);
+end
