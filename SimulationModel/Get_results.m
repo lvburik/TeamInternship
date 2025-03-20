a3454395127eb49e850d7fa1d277f6b933ddb1f6
Luc van Burik <l.j.v.burik@student.tue.nl>
2025-03-20 13:17:56 +0100

    Add column labels to csv file and remove plot

--- SimulationModel/Get_results.m
+++ SimulationModel/Get_results.m
@@ -1,21 +1,26 @@
-%create plot and show plot (not needed)
-pdeplot3D(ThermalModel,"ColorMapData",thermalresults.Temperature(:,end))
-colormap("gray")
-clim([22 55])
-view(90, -90)
-zoom(1)
+%%process results into labeled csv file
 
-%find all nodes in the XY plane
-nodes = find(ThermalModel.Mesh.Nodes(3, :) == 0)';
+function Get_results(ThermalModel, tlist, ThermalResults, filename)
 
-%extract all temperature values for these nodes
-temps = thermalresults.Temperature(nodes, :);
+    %find all nodes in the XY plane
+    node_IDs = find(ThermalModel.Mesh.Nodes(3, :) == 0)';
 
-%find location of nodes
-locs = ThermalModel.Mesh.Nodes(1:2, nodes)';
+    %extract all temperature values for these nodes
+    TemperatureData = ThermalResults.Temperature(node_IDs, :);
 
-%combine node IDs, location and temperature data into one matrix
-results = [nodes locs temps];
+    %find location of nodes
+    locs = ThermalModel.Mesh.Nodes(1:2, node_IDs)';
+    X = locs(:, 1); Y = locs(:, 2);
 
-%save CSV (first column = node_ID, second column = X, third column = Y, remaining columns belong to each timestamp)
-csvwrite('results.csv', results)
+    TimestampStrings = "T_" + string(tlist);
+
+    %create a table
+    DataTable = array2table([node_IDs, X, Y, TemperatureData], ...
+        'VariableNames',['Node_ID', 'X', 'Y', TimestampStrings]);
+
+    %save table
+    writetable(DataTable, filename)
+
+end
+
+
