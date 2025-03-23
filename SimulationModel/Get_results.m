592148877067900ea30871519e5c642d9bc4bc8c
kpenners <k.penners@student.tue.nl>
2025-03-23 19:30:48 +0100

    corrected node selection in get_results

--- SimulationModel/Get_results.m
+++ SimulationModel/Get_results.m
@@ -3,7 +3,7 @@
 function Get_results(ThermalModel, tlist, ThermalResults, filename)
 
     %find all nodes in the XY plane
-    node_IDs = find(ThermalModel.Mesh.Nodes(3, :) == 0)';
+    node_IDs = find(ThermalModel.Mesh.Nodes(3, :) == 0.02)';
 
     %extract all temperature values for these nodes
     TemperatureData = ThermalResults.Temperature(node_IDs, :);
@@ -20,6 +20,12 @@ function Get_results(ThermalModel, tlist, ThermalResults, filename)
 
     %save table
     writetable(DataTable, filename)
+
+    figure;
+    scatter(X, Y, 10, 'filled'); % Small dots, filled markers
+    title('Node Locations in XY Plane');
+    axis equal; % Keep aspect ratio
+    grid on;
 
 end
 
