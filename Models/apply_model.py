1d8fc0a4f0094217804b8a3cae3de8c1d7b528cc
kpenners <k.penners@student.tue.nl>
2025-04-30 18:50:40 +0200

    update apply_model

--- Models/apply_model.py
+++ Models/apply_model.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from thermal_dataset import ThermalDataset 
 
 # load the model
-rf = joblib.load("./Models/random_forest_model.joblib")
+rf = joblib.load("./Models/rf1.joblib")
 
 # choose a test video path
 file_path = "fft data/Composite/new_2_lamps_left_off_fft.npy"
@@ -13,19 +13,8 @@ file_path = "fft data/Resin/rec_1_lamp_top_on_fft.npy"
 file_path = "fft data/Composite/new_2_lamps_right_off_fft.npy"
 file_path = "fft data/Resin/rec_2_lamps_right_off_fft.npy"
 file_path = "fft data/Resin/circular_3_lamps_fft.npy"
+
 file_paths = [
-    "fft data/Composite/new_2_lamps_top_off_fft.npy",
-    "fft data/Composite/new_3_lamps_fft.npy",
-    "fft data/Resin/circular_1_lamp_left_on_fft.npy",
-    "fft data/Resin/circular_1_lamp_right_on_fft.npy",
-    "fft data/Resin/circular_1_lamp_top_on_fft.npy",
-    "fft data/Resin/circular_2_lamps_left_off_fft.npy",
-    "fft data/Resin/circular_3_lamps_fft.npy",
-    "fft data/Resin/rec_1_lamp_left_on_fft.npy",
-    "fft data/Resin/rec_1_lamp_right_on_fft.npy",
-    "fft data/Resin/rec_2_lamps_left_off_fft.npy",
-    "fft data/Resin/rec_2_lamps_right_off_fft.npy",
-    "fft data/Resin/rec_2_lamps_top_off_fft.npy",
     "fft data/Composite/new_1_lamp_left_on_fft.npy",
     "fft data/Composite/new_1_lamp_right_on_fft.npy",
     "fft data/Composite/new_1_lamp_top_on_fft.npy",
@@ -36,8 +25,7 @@ file_paths = [
     "fft data/Resin/square_2_lamps_left_off_fft.npy",
     "fft data/Resin/square_2_lamps_right_off_fft.npy",
     "fft data/Resin/triangular_1_lamp_right_on_fft.npy",
-    "fft data/Resin/triangular_2_lamps_left_off_fft.npy"
-]
+    "fft data/Resin/triangular_2_lamps_left_off_fft.npy"]
 
 mask_map = {
         "circular" : "Resin plates/circular.png",
@@ -59,6 +47,7 @@ for file_path in file_paths:
         mask_map=mask_map,
         num_pixels=307200,
         extract_peaks=False,
+        extract_patches=True,
         cutoff_frequency=0.1
     )
 
@@ -71,12 +60,12 @@ for file_path in file_paths:
     preds = rf.predict(fft_data)
 
     # reshape to image
-    pred_img = preds.reshape((480, 640))
+    pred_img = preds.reshape((480 , 640))
 
     # visualize predictions
     plt.figure(figsize=(8, 6))
     plt.imshow(pred_img, cmap='hot')
-    plt.title(f"predicted defect map\n{file_path}")
+    plt.title(f"predicted defect map")
     plt.colorbar(label="Class")
     plt.axis("off")
     plt.tight_layout()
@@ -84,7 +73,7 @@ for file_path in file_paths:
 
     # save plot
     file_name = file_path.split('/')[-1].replace('.npy', '.png').replace('_fft', '')
-    output_file = f"pred_{file_name}"
+    output_file = f"pred1_{file_name}"
     plt.savefig(output_file, dpi=300)
     print(f"saved prediction to {output_file}")
 
