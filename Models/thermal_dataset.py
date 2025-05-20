e446d3acbe11a3a73396987158ead17d189bd933
kpenners <k.penners@student.tue.nl>
2025-05-20 14:24:46 +0200

    clean up

--- Models/thermal_dataset.py
+++ Models/thermal_dataset.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset
 from matplotlib import pyplot as plt
 from PIL import Image
 
+# add the path to the preprocessing module
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Preprocessing')))
 from preprocessing import *
 
@@ -14,31 +15,31 @@ class ThermalDataset(Dataset):
                  add_zero_padding=False, apply_fft=False, cutoff_frequency=1,
                  apply_PCA=False, extract_patches=False, extract_cnn_patches=False):
         
-        self.file_paths = file_paths
-        self.data_dir = data_dir
-        self.center_data = center_data
-        self.add_zero_padding = add_zero_padding
-        self.apply_fft = apply_fft
-        self.cutoff_frequency = cutoff_frequency
-        self.apply_PCA = apply_PCA
-        self.extract_patches = extract_patches
-        self.extract_cnn_patches = extract_cnn_patches
+        self.file_paths = file_paths                    # list of file paths within data_dir
+        self.data_dir = data_dir                        # path to data directory    
+        self.center_data = center_data                  # center the data
+        self.add_zero_padding = add_zero_padding        # zeropad time series
+        self.apply_fft = apply_fft                      # apply fft to data
+        self.cutoff_frequency = cutoff_frequency        # cutoff frequency for fft
+        self.apply_PCA = apply_PCA                      # apply PCA to data
+        self.extract_patches = extract_patches          # extract patches from input image
+        self.extract_cnn_patches = extract_cnn_patches  # extract patches for CNN training
 
         # mapping of masks to corresponding files
         self.mask_mapping = mask_map
 
     def __len__(self):
+        # number of files in dataset
         return len(self.file_paths)
     
     def __getitem__(self, idx):
-        
         # load data from file  
         file_path = os.path.join(self.data_dir, self.file_paths[idx])
         data = np.load(file_path, mmap_mode='r').copy()
         #print(f"loaded {self.file_paths[idx]}")
         #print("data shape: ", data.shape)
 
-        # load mask (labels)
+        # load corresponding mask (labels)
         file_name = os.path.basename(file_path)
         if "Composite" in file_path:
             mask_path = os.path.join(self.data_dir, "Masks", self.mask_mapping["composite"])
@@ -60,7 +61,7 @@ class ThermalDataset(Dataset):
         #print(f"loaded mask with shape: {mask.shape}")
 
         mask = mask.reshape(480, 640)
-        mask = np.where(mask > 0, 1, 0)
+        mask = np.where(mask > 0, 1, 0) # binary mask
         
         # plot mask
         """plt.imshow(mask, cmap='gray')
@@ -68,16 +69,14 @@ class ThermalDataset(Dataset):
         plt.title('Defect Mask')
         plt.show()"""
 
-        # center the data
+        # optional preprocessing steps
         if self.center_data:
             data -= np.mean(data, axis=0)
 
-        # add zero padding
         if self.add_zero_padding:
             data = add_zero_padding(data, 2048 - data.shape[0])
             print("zero padded data shape: ", data.shape)
 
-        # apply fft
         if self.apply_fft:
             t = np.linspace(0.5, data.shape[0]*0.5, data.shape[0])
             fft_data, freq = apply_fft(data, t)
@@ -96,20 +95,17 @@ class ThermalDataset(Dataset):
 
             return fft_data, mask, freq
         
-        # extract patches from fft data
         if self.extract_patches:
             data, mask = extract_patches(data, mask, patch_size=4)
             data = data.T
             print("extracted patches shape: ", data.shape)
             print("extracted patches mask shape: ", mask.shape)
         
-        # extract patches from fft data for CNN 
         if self.extract_cnn_patches:
             data, mask = extract_cnn_patches(data, mask, patch_size=128, overlap=0.5, neg_patch_prob=1)
             #print("extracted cnn patches shape: ", data.shape)
             #print("extracted cnn patches mask shape: ", mask.shape)
         
-        # apply PCA to reduce dimensionality
         if self.apply_PCA:
             data = apply_PCA_SVD(data, num_components=10)
             print("PCA data shape: ", data.shape)
