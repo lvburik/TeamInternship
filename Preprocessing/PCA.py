f2f4866f6d9e6742a174f66fb1a0854770219f65
bpetkova13 <b.d.petkova@student.tue.nl>
2025-06-09 22:30:39 +0200

    Update PCA.py

--- Preprocessing/PCA.py
+++ Preprocessing/PCA.py
@@ -32,26 +32,14 @@ for l in label:
 
 
 
-def PCA(X , num_components):
-     
-    #Step-1
+def PCA(X , num_components): #code for implementing PCA, however it runs slowly
     X_meaned = X - np.mean(X , axis = 1, keepdims=True)
-     
-    #Step-2
     cov_mat = np.cov(X_meaned , rowvar = False)
-     
-    #Step-3
     eigen_values , eigen_vectors = np.linalg.eigh(cov_mat)
-     
-    #Step-4
     sorted_index = np.argsort(eigen_values)[::-1]
     sorted_eigenvalue = eigen_values[sorted_index]
     sorted_eigenvectors = eigen_vectors[:,sorted_index]
-     
-    #Step-5
     eigenvector_subset = sorted_eigenvectors[:,0:num_components]
-     
-    #Step-6
     X_reduced = np.dot(X_meaned, eigenvector_subset.T)
 
      
@@ -60,17 +48,10 @@ def PCA(X , num_components):
 
 import numpy as np
 
-def PCA_SVD(X, num_components):
-    # Step-1: Mean center the data
+def PCA_SVD(X, num_components): #code for implementing PCA that was used, uses SVD for faster computation
     X_meaned = X - np.mean(X, axis=0)
-    
-    # Step-2: Compute SVD instead of covariance matrix
     U, S, Vt = np.linalg.svd(X_meaned, full_matrices=False)
-    
-    # Step-3: Select the top 'num_components' principal components
     eigenvector_subset = Vt[:num_components, :]  # Shape: (num_components, 307200)
-    
-    # Step-4: Project data onto the new subspace
     X_reduced = np.dot(X_meaned, eigenvector_subset.T)  # Shape: (301, num_components)
     
     return X_reduced
